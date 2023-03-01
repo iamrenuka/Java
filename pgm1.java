82e47715371aeb273136ad6758175df193c70795
iamrenuka <126648318+iamrenuka@users.noreply.github.com>
2023-03-01 12:34:55 +0530

    Update pgm1.java

--- pgm1.java
+++ pgm1.java
@@ -2,10 +2,10 @@ public class QuotientRemainder {
 
   public static void main(String[] args) {
 
-    int dividend = 25, divisor = 4;
+    int div = 40, divisor = 4;
 
-    int quotient = dividend / divisor;
-    int remainder = dividend % divisor;
+    int quotient = div / divisor;
+    int remainder = div % divisor;
     System.out.println("Quotient = " + quotient);
     System.out.println("Remainder = " + remainder);
   }
